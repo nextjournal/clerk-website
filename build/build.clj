9eca8ebb247b810d8e5be72f2f524cf15ba63409
Andrea Amantini <lo.zampino@gmail.com>
2022-10-31 17:55:17 +0100

    Assert npx

--- build/build.clj
+++ build/build.clj
@@ -3,8 +3,9 @@
             [clojure.java.shell :refer [sh]]))
 
 (defn build! [opts]
+  (assert (= 0 (:exit (sh "which" "npm"))) "Node needs to be installed")
+  (assert (= 0 (:exit (sh "which" "npx"))) "`npx` needs to be available")
   (println "Installing tailwind…")
-  (assert (= 0 (:exit (sh "which" "npm"))) "Node need to be installed")
   (sh "npm" "install" "-D" "tailwindcss" "@tailwindcss/typography")
   #_ (sh "npx" "tailwindcss" "--help")
   (clerk/build! opts))
