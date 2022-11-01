25704b3c402dabdeac02b60e31700fe7d32572a1
Andrea Amantini <lo.zampino@gmail.com>
2022-11-01 12:18:20 +0100

    Remove build step (Garden should have tailwind now)

--- build/build.clj
+++
@@ -1,9 +0,0 @@
-(ns build
-  (:require [nextjournal.clerk :as clerk]
-            [clojure.java.shell :refer [sh]]))
-
-(defn build! [opts]
-  (println "Installing tailwind…")
-  (sh "npm" "install" "-D" "tailwindcss" "@tailwindcss/typography")
-  (println (:out (sh "npx" "tailwindcss" "--help")))
-  (clerk/build! opts))
