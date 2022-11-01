86d5d7166a68e30ee11898ed721c0f337fb4077d
Andrea Amantini <lo.zampino@gmail.com>
2022-11-01 11:08:40 +0100

    remove which

--- build/build.clj
+++ build/build.clj
@@ -3,9 +3,7 @@
             [clojure.java.shell :refer [sh]]))
 
 (defn build! [opts]
-  (assert (= 0 (:exit (sh "which" "npm"))) "Node needs to be installed")
-  (assert (= 0 (:exit (sh "which" "npx"))) "`npx` needs to be available")
   (println "Installing tailwind…")
   (sh "npm" "install" "-D" "tailwindcss" "@tailwindcss/typography")
-  #_ (sh "npx" "tailwindcss" "--help")
+  (println (:out (sh "npx" "tailwindcss" "--help")))
   (clerk/build! opts))
